ea7a42d6ccc711c6fdd4ac0b7920cdea6aa1aa96
chrisinko <128570604+chrisinko@users.noreply.github.com>
2026-03-25 14:50:39 +1100

    Prac wk5

--- src/comp3170/week5/Scene.java
+++ src/comp3170/week5/Scene.java
@@ -1,5 +1,6 @@
 package comp3170.week5;
 
+import org.joml.Vector3f;
 import org.joml.Vector4f;
 import comp3170.InputManager;
 import comp3170.SceneObject;
@@ -11,11 +12,14 @@ public class Scene extends SceneObject {
 	
 	public Scene() {
 		camera = new Camera();
-		createFlower(new Vector4f(0.0f,0.0f,0.f,1.0f));		
+		createFlower(new Vector4f(0.0f,0.0f,0.f,1.0f));
+		createFlowerHead(new Vector4f(0.0f, 1.8f, 0.0f, 1.0f));
 	}
 	
 	public Camera sceneCam() {
+		camera.setParent(null);
 		return camera;
+		
 	}
 	
 	public void createFlower(Vector4f position) {
@@ -23,6 +27,13 @@ public class Scene extends SceneObject {
 		flower.setParent(this);	
 		flower.getMatrix().translate(position.x,position.y,0.0f);
 	}
+	
+	public void createFlowerHead(Vector4f position) {
+		FlowerHead fh = new FlowerHead(20);
+		fh.setParent(this);
+		fh.getMatrix().translate(position.x, position.y, 0.0f);
+	}
+	
 
 	public void update(InputManager input, float dt) {
 		
